ccfa7c66e2cb1f73c860c7a18064db3124cd6eb0
AhmadMBahattab <xpazzax11@gmail.com>
2022-02-27 12:31:23 +0300

    Add random buttons

--- app/screens/RandomPlayers.js
+++ app/screens/RandomPlayers.js
@@ -1,12 +1,29 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 
 const RandomPlayers = () => {
   return (
-    <View>
-      <Text>RandomPlayers</Text>
-    </View>
+    <>
+      <TouchableOpacity>
+        <View style={styles.quickButton}>
+          <Text style={{ color: "white", fontSize: 18 }}>دقة ولد سريعة</Text>
+        </View>
+      </TouchableOpacity>
+
+      <View style={styles.playersNamesContainer}></View>
+    </>
   );
 };
+const styles = StyleSheet.create({
+  quickButton: {
+    margin: 20,
+    backgroundColor: "#004582",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 40,
+    borderRadius: 15,
+  },
+  playersNamesContainer: {},
+});
 
 export default RandomPlayers;
