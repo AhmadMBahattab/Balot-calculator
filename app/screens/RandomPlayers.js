46f000f83820a81cbbbcd581eb8016c7a001e4bd
Ahmad mohammed <xpazzax11@gmail.com>
2022-02-27 18:46:15 +0300

    Finsh onChange function & add player.

--- app/screens/RandomPlayers.js
+++ app/screens/RandomPlayers.js
@@ -1,4 +1,10 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import React, { useState } from "react";
 import { Input, Button, FAB } from "react-native-elements";
 import { FontAwesome } from "@expo/vector-icons";
@@ -12,9 +18,20 @@ const RandomPlayers = () => {
     if (playerName.length < 1) {
       return;
     }
-    
+
+    if (playersArray.length >= 10) {
+      return;
+    }
+
+    newArray.push({
+      id: playersArray.length + 1,
+      name: playerName,
+    });
+    setplayerName("");
+    setplayersArray(newArray);
+    console.log(playersArray.length);
   };
-  console.log(1 + " " + playerName);
+
   return (
     <>
       <TouchableOpacity>
@@ -32,6 +49,7 @@ const RandomPlayers = () => {
             onChangeText={(name) => {
               setplayerName(name);
             }}
+            value={playerName}
             maxLength={15}
           />
         </View>
@@ -45,7 +63,26 @@ const RandomPlayers = () => {
           />
         </View>
       </View>
-      <View style={styles.playersNames}></View>
+      <ScrollView style={styles.playersNames}>
+        {playersArray.map((item) => (
+          <View
+            style={{ justifyContent: "space-between", flexDirection: "row" }}
+          >
+            <Text>ss</Text>
+            <Text style={{ fontSize: 20, marginTop: 5 }}>
+              {item.id} : {item.name}
+            </Text>
+            <View></View>
+          </View>
+        ))}
+      </ScrollView>
+      <View style={styles.setTeamsButtonContainer}>
+        <TouchableOpacity>
+          <View style={styles.quickButton}>
+            <Text style={{ color: "white", fontSize: 18 }}>دقة الولد</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </>
   );
 };
@@ -64,6 +101,13 @@ const styles = StyleSheet.create({
   },
   addNameContainer: {
     justifyContent: "center",
+  },
+  playersNames: {
+    padding: 20,
+  },
+  setTeamsButtonContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
   },
 });
 
